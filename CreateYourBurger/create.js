5dee8d9b47f002d71af874d5094b4a7770b2f954
vitalii <2456vitalic@gmail.com>
2022-05-19 15:38:22 +0300

    Edit errors

--- CreateYourBurger/create.js
+++ CreateYourBurger/create.js
@@ -1,9 +1,17 @@
 'use strict'
 const bread = document.querySelector('.chifla')
 const meat = document.querySelector('.carne')
-const sosTop= document.querySelector('.sos-top')
+const sosTop= document.querySelector('.top-top')
 const bottom = document.querySelector('.sos-bottom')
 const cheese = document.querySelector('.cascaval')
 const cosh = document.querySelector('.cosh')
+let boxCosh = []
+
+cosh.addEventListener('click',()=>{
+    boxCosh.push(bread.value , meat.value , sosTop.value , bottom.value , cheese.value)
+    alert(boxCosh)
+})
+
+
 
 
